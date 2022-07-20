dc095d9b109fb7de4fc4af7119da451539253ae4
Stephan <sgriesel@gmail.com>
2022-07-20 17:01:30 +0200

    update setup - wip

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App bg-bkg text-white">
+      <h1 className='text-2xl text-white'>Hello this is a test</h1>
+    </div>
+  )
+}
+
+export default App
