a94250b50cd8aa0b5fe388e18278807b7bdec748
Stephan <sgriesel@gmail.com>
2022-08-02 06:39:42 +0200

    icon & shadows add

--- src/App.jsx
+++ src/App.jsx
@@ -82,10 +82,10 @@ function App() {
         </nav>
         <header className='min-h-screen flex flex-col justify-evenly items-center relative'>
           <img className="absolute h-full w-full object-cover object-center -z-5" src={header.hero.url} aria-hidden="true" />
-          <h1 className="text-5xl text-center z-10 font-bold tracking-wide py-12 px-20">{header.titleText}</h1>
-          <a href={header.heroUrl} className="bg-accent text-bkg font-medium py-3 px-8 rounded-full border border-bkg focus:outline-none z-10 focus-visible:ring-4 ring-accent ring-offset-bkg ring-offset-2 hover:bg-accent/90">
+          <h1 className="text-5xl text-center z-10 font-bold tracking-wide py-12 px-96 drop-shadow-text-sm lg:drop-shadow-text-lg">{header.titleText}</h1>
+          <a href={header.heroUrl} className="bg-accent text-bkg font-medium py-3 px-8 rounded-full border border-bkg focus:outline-none z-10 focus-visible:ring-4 ring-accent ring-offset-bkg ring-offset-2 hover:bg-accent/90 flex space-x-2 drop-shadow-text-lg hover:drop-shadow-none transition-shadow">
             <span className="uppercase tracking-wide">{header.ctaText}</span>
-          </a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="32" y1="216" x2="208" y2="216" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><path d="M83.3,216A88,88,0,0,1,32,136V88H208v48a88,88,0,0,1-51.3,80" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M208,88h4a32,32,0,0,1,32,32v8a32,32,0,0,1-32,32h-7.4" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><line x1="80" y1="24" x2="80" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="120" y1="24" x2="120" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="160" y1="24" x2="160" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>          </a>
         </header>
       </div>
     </div>
@@ -93,6 +93,3 @@ function App() {
 }
 
 export default App;
-
-// set up contentful
-// create query for header, content, footer
