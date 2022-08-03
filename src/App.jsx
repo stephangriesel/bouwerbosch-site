5cefc7ecdd16d3fe6372dc0b8ceb0fa025f11cac
Stephan <sgriesel@gmail.com>
2022-08-03 07:31:00 +0200

    heade style, query update, main section data render

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,14 @@ const query = `
       heroUrl
       titleText
       titleDescription
-      ctaText
+      ctaText 
+    }
+  }
+  mainCollection {
+    items {
+      smallIntro
+      headlineIntro
+      paragraphIntro
     }
   }
 }
@@ -31,6 +38,9 @@ function App() {
   // define the initial state
   const [header, setHeader] = useState(null);
   console.log('check header state:', header);
+
+  const [main, setMain] = useState(null);
+  console.log('check main state:', main);
 
   useEffect(() => {
     window
@@ -56,15 +66,16 @@ function App() {
 
         // rerender the entire component with new data
         setHeader(data.headerCollection.items[0]);
+        setMain(data.mainCollection.items[0]);
       });
   }, []);
 
   // show a loading screen case the data hasn't arrived yet
-  if (!header) {
+  if (!header || !main) {
     return '';
   }
   return (
-    <div className='App bg-bkg text-white selection:bg-accent selection:text-bkg overflow-hidden'>
+    <div className='App bg-bkg text-white selection:bg-accent selection:text-bkg'>
       <div id='parallax'>
         <nav
           className='grid place-items-center absolute w-full top-0 z-50 drop-shadow-text-sm'
@@ -79,14 +90,27 @@ function App() {
               <img src={header.logo.url} alt="logo" />
             </div>
           </a>
-        </nav>
+        </nav>  
         <header className='min-h-screen flex flex-col justify-evenly items-center relative'>
+          <div className="absolute bg-gradient-to-b inset-0 bottom-3/4 from-accent to-transparent"></div>
+          <div className="absolute bg-gradient-to-t inset-0 top=1/3 -bottom-32 from-bkg/80 to-transparent"></div>
           <img className="absolute h-full w-full object-cover object-center -z-5" src={header.hero.url} aria-hidden="true" />
           <h1 className="text-5xl text-center z-10 font-bold tracking-wide py-12 px-96 drop-shadow-text-sm lg:drop-shadow-text-lg">{header.titleText}</h1>
-          <a href={header.heroUrl} className="bg-accent text-bkg font-medium py-3 px-8 rounded-full border border-bkg focus:outline-none z-10 focus-visible:ring-4 ring-accent ring-offset-bkg ring-offset-2 hover:bg-accent/90 flex space-x-2 drop-shadow-text-lg hover:drop-shadow-none transition-shadow">
+          <a href={header.heroUrl} className="bg-accent text-bkg font-medium py-3 px-8 rounded-full border border-bkg focus:outline-none z-10 focus-visible:ring-4 ring-accent ring-offset-bkg ring-offset-2 hover:bg-accent/90 flex space-x-2 drop-shadow-text-lg hover:drop-shadow-none transition-shadow items-center">
             <span className="uppercase tracking-wide">{header.ctaText}</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="32" y1="216" x2="208" y2="216" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><path d="M83.3,216A88,88,0,0,1,32,136V88H208v48a88,88,0,0,1-51.3,80" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M208,88h4a32,32,0,0,1,32,32v8a32,32,0,0,1-32,32h-7.4" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><line x1="80" y1="24" x2="80" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="120" y1="24" x2="120" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="160" y1="24" x2="160" y2="48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>          </a>
         </header>
+        <main className="relative mt-16 sm:mt-24 lg:mt-40 pb-16 -mb-16 grid gap-16 sm:gap-24 lg:gap-40 overflow-hidden">
+          <section aria-labelledby="headline" class="container grid gap-4 text-center max-w-prose">
+            <div>
+              <small className="tracking-widest text-accent uppercase drop-shadow-text-sm">{main.smallIntro}</small>
+              <h2 id="headline" className="text-3xl font-bold tracking-wide drop-shadow-md">{main.headlineIntro}</h2>
+            </div>
+            <p className="text-muted drop-shadow-text-sm">
+              {main.paragraphIntro}
+            </p>
+          </section>
+        </main>
       </div>
     </div>
   );
