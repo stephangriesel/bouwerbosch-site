5194b9924ef2334756f5a397f7cf245da4e4b358
Stephan Griesel <sgriesel@gmail.com>
2022-09-22 19:12:05 +0200

    remove fade in plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -46,20 +46,20 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    plugin(( { addUtilities, theme }) => {
-      addUtilities({
-        ".fade-up": {
-          transition:
-            "transform 1s cubic-bezier(0.64, 0.04, 0.26, 0.87), opacity 0.8s cubic-bezier(0.64, 0.04, 0.26, 0.87)",
-          opacity: theme("opacity.0"),
-          transform: "translate3d(0, 2rem, 0)",
-        },
-        ".faded": {
-          opacity: theme("opacity.100"),
-          transform: "translate3d(0, 0, 0)",
-        },
-      });
-    })
-  ],
+  // plugins: [
+  //   plugin(( { addUtilities, theme }) => {
+  //     addUtilities({
+  //       ".fade-up": {
+  //         transition:
+  //           "transform 1s cubic-bezier(0.64, 0.04, 0.26, 0.87), opacity 0.8s cubic-bezier(0.64, 0.04, 0.26, 0.87)",
+  //         opacity: theme("opacity.0"),
+  //         transform: "translate3d(0, 2rem, 0)",
+  //       },
+  //       ".faded": {
+  //         opacity: theme("opacity.100"),
+  //         transform: "translate3d(0, 0, 0)",
+  //       },
+  //     });
+  //   })
+  // ],
 }
